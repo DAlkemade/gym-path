19f1a0fc587e6399cd458f38ecf71ae08480ef52
Daan <28051445+DAlkemade@users.noreply.github.com>
2020-03-10 09:47:19 +0000

    Create scaffold for environment creation using cartpole environment

---
+++ gym_path/__init__.py
@@ -0,0 +1,10 @@
+from gym.envs.registration import register
+
+register(
+    id='PathFollower-v0',
+    entry_point='gym_path.envs:PathEnv',
+)
+register(
+    id='PathFollower-extension-v0',
+    entry_point='gym_path.envs:PathExtensionEnv',
+)
---
+++ gym_path/envs/__init__.py
@@ -0,0 +1,2 @@
+from gym_path.envs.path_env import PathEnv
+from gym_path.envs.path_extension_env import PathEnvExtension
